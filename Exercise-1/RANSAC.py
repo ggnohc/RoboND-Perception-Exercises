1049077ea610b82824f01e2e5a3550f2cb02394d
ggnohc <gheegnohc@gmail.com>
2017-10-03 10:44:34 +0800

    Updated Voxel grid filter portion.

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -6,6 +6,20 @@ cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 
 # Voxel Grid filter
+# Create a VoxelGrid filter object for input point cloud
+vox = cloud.make_voxel_grid_filter()
+
+# Choose a voxel (leaf) size
+LEAF_SIZE = 1
+LEAF_SIZE = 0.1
+
+# Set the voxel (leaf) size
+vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+# Call the filter function to obtain the resultant downsampled point cloud
+cloud_filtered = vox.filter()
+filename = 'voxel_downsampled.pcd'
+pcl.save(cloud_filtered, filename)
 
 
 # PassThrough filter
@@ -24,5 +38,3 @@ cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 
 # Save pcd for tabletop objects
-
-
