7c241fcc94a1e3fa2c89b03ef4588eea686b3edf
ggnohc <gheegnohc@gmail.com>
2017-10-06 17:03:24 +0800

    Filled in some of the TODOs.

--- Exercise-2/sensor_stick/scripts/segmentation.py
+++ Exercise-2/sensor_stick/scripts/segmentation.py
@@ -9,15 +9,67 @@ from pcl_helper import *
 def pcl_callback(pcl_msg):
 
     # TODO: Convert ROS msg to PCL data
+    pcl_data = ros_to_pcl(pcl_msg)
+
 
     # TODO: Voxel Grid Downsampling
+    # Voxel Grid filter
+    # Create a VoxelGrid filter object for input point cloud
+    vox = pcl_data.make_voxel_grid_filter()
+
+    # Choose a voxel (leaf) size
+    # LEAF_SIZE = 1
+    LEAF_SIZE = 0.01  #this gives the best results
+
+    # Set the voxel (leaf) size
+    vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+    # Call the filter function to obtain the resultant downsampled point cloud
+    pcl_data_filtered = vox.filter()
+    filename = 'voxel_downsampled.pcd'
+    pcl.save(pcl_data_filtered, filename)
 
     # TODO: PassThrough Filter
+    # Create a PassThrough filter object.
+    passthrough = pcl_data_filtered.make_passthrough_filter()
 
+    #Assign axis and range to the passthrough filter object.
+    filter_axis = 'z'
+    passthrough.set_filter_field_name(filter_axis)
+    axis_min = 0
+    axis_max = 2
+    passthrough.set_filter_limits(axis_min, axis_max)
+
+    #Finally use the filter function to obtain the resultant point cloud.
+    pcl_data_filtered = passthrough.filter()
+    filename = 'pass_through_filtered.pcd'
+    pcl.save(pcl_data_filtered, filename)
 
     # TODO: RANSAC Plane Segmentation
+    # Create the segmentation object
+    seg = pcl_data_filtered.make_segmenter()
+
+    # Set the model you wish to fit
+    seg.set_model_type(pcl.SACMODEL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+
+    # Max distance for a point to be considered fitting the model
+    max_distance = 0.01
+    seg.set_distance_threshold(max_distance)
 
     # TODO: Extract inliers and outliers
+    # Call the segment function to obtain set of inlier indices and model coefficients
+    inliers, coefficients = seg.segment()
+
+    # Extract inliers
+    extracted_inliers = pcl_data_filtered.extract(inliers, negative=False)
+    filename = 'extracted_inliers.pcd'
+    pcl.save(extracted_inliers, filename)
+
+    # Extract outliers
+    extracted_outliers = pcl_data_filtered.extract(inliers, negative=True)
+    filename = 'extracted_outliers.pcd'
+    pcl.save(extracted_outliers, filename)
 
     # TODO: Euclidean Clustering
 
