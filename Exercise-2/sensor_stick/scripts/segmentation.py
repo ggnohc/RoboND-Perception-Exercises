ed4a0fb0b15de876fef20ce2bc8f108029d251e5
ggnohc <gheegnohc@gmail.com>
2017-10-08 18:45:38 +0800

    Fix axis_min and axis_max

--- Exercise-2/sensor_stick/scripts/segmentation.py
+++ Exercise-2/sensor_stick/scripts/segmentation.py
@@ -36,8 +36,8 @@ def pcl_callback(pcl_msg):
     #Assign axis and range to the passthrough filter object.
     filter_axis = 'z'
     passthrough.set_filter_field_name(filter_axis)
-    axis_min = 0
-    axis_max = 2
+    axis_min = 0.6
+    axis_max = 1.1
     passthrough.set_filter_limits(axis_min, axis_max)
 
     #Finally use the filter function to obtain the resultant point cloud.
